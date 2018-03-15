022ccdabd0bab20bd775f616e1998b633d03c55b
kzn17qr <kzn17qr@gmail.com>
2018-03-16 01:14:59 +0900

    Add Back button

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 // src/app/App.js
 
+import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import HomeScreen from './components/HomeScreen';
 import EditScreen from './components/EditScreen';
 
-const App = StackNavigator(
+const RootStack = StackNavigator(
   {
     Home: { screen: HomeScreen },
     Edit: { screen: EditScreen },
@@ -13,5 +14,7 @@ const App = StackNavigator(
     initialRouteName: 'Home',
   },
 );
+
+const App = () => <RootStack />;
 
 export default App;
--- src/app/components/EditScreen/index.js
+++ src/app/components/EditScreen/index.js
@@ -1,7 +1,8 @@
 // src/app/components/EditScreen/index.js
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,10 +15,17 @@ const styles = StyleSheet.create({
   },
 });
 
-const EditScreen = () => (
+const EditScreen = props => (
   <View style={styles.container}>
     <Text style={styles.text}>Edit Screen</Text>
+    <TouchableOpacity onPress={() => props.navigation.goBack()}>
+      <Text>Back</Text>
+    </TouchableOpacity>
   </View>
 );
+
+EditScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
 
 export default EditScreen;
